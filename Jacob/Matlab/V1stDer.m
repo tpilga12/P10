021bc984b8a72ba8c5bd2532662a2f0f82d2c24c
Naundrup <jnpe12@student.aau.dk>
2018-02-22 16:20:02 +0100

    Matlab

--- Jacob/Matlab/V1stDer.m
+++ Jacob/Matlab/V1stDer.m
@@ -10,6 +10,7 @@ A1 = d^2/4 * acos(((d/2)-h1/(d/2)))-sqrt(h*(d-h1))*((d/2)-h1);
 H = (2*(1-Theta)*Q1-2*(1-Theta)*Q1+2*Theta*Q1+2*Theta*Q1)*Dt/Dx - A1+A1+A1;
 
 
-
-f = -72*(d/4)^0.635 * pi*(d/2)^2*Ie^0.5*(0.46-0.5*cos(pi*h/d)+0.04*cos(2*pi*h/d))*(Dt/Dx)-1/(2*Theta)*(h*2*sqrt(-h^2+h*d)-H);
+ 
+f = -72*(d/4)^0.635 * pi*(d/2)^2*Ie^0.5*(0.46-0.5*cos(pi*h/d)+0.04*cos(2*pi*h/d))*(Dt/Dx)-1/(2*Theta)*((d^2/4 * acos(((d/2)-h1/(d/2)))-sqrt(h*(d-h1))*((d/2)-h1))-H);
+%f = -72*(d/4)^0.635 * pi*(d/2)^2*Ie^0.5*(0.46-0.5*cos(pi*h/d)+0.04*cos(2*pi*h/d))*(Dt/Dx)-1/(2*Theta)*(h*2*sqrt(-h^2+h*d)-H);
 end 
