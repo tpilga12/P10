dff386751d76436182669d43a49a25880043e279
naundrup <jnpe12@student.aau.dk>
2018-05-11 08:23:26 +0200

    æatex

--- Matlab/simulation/functions/MPC_controller.m
+++ Matlab/simulation/functions/MPC_controller.m
@@ -1,17 +1,29 @@
 %% MPC controller
-clc,clear all
+%clc,clear all
 
-% MPCobj = mpc(lin_sys,20);
-% T = 20;
-% r = [0; 0.03];
 
-A =2;
-Hp = 10;
-for n = 1:Hp
-    Agrafical(n) = [A^n];
-    Agrafical =  Agrafical';
-    
-  
-    
-    
+Hp = 10; % Prediction horizon
+Hu = 9;  % Control horizon
+XHp = int8(2:Hp);
+XHu = int8(2:Hu);
+for n = 1:Hp %%% Lifted A matrix
+    if n == 1
+        Alifted = [lin_sys.A^n];
+    else
+        Alifted(end+1:end+length(lin_sys.A),:) = [lin_sys.A^n];
+    end 
 end
+
+for n = 1:Hp %%% Lifted B matrix
+    
+    if n == 1
+        Blifted = [lin_sys.B];
+    else      
+            Blifted(end+1:end+length(lin_sys.B),:) = [lin_sys.A^n*lin_sys.B];
+    end
+end
+
+
+H = theta'*Q*theta;
+f = x(k)'*psi'Q*theta
+
