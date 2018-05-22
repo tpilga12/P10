5468aed141be004cb1edee72a425a20e69260185
bob <tpilga12@student.aau.dk>
2018-05-22 23:44:54 +0200

    matlab

--- Matlab/simulation/functions/init_pipe.m
+++ Matlab/simulation/functions/init_pipe.m
@@ -41,6 +41,7 @@ while abs(avg-desired) > limit
             %%%%% TEST AREA :DDD %%%%%
             data{x}.lut.Q = Q_initialize;
             data{x}.lut.h = h_init;
+            data{x}.lut.limit = Qf;
             %%%%%%%%%%%%%%%%%%%%%%%%%%
             
             data{x}.fitfunc = fit(Q_initialize',h_init','poly9');
@@ -58,18 +59,21 @@ while abs(avg-desired) > limit
                 if x == 1
                     data{x}.Q(1,1:sections) = input.Q_init(sys_component);
                     data{x}.C(1,1:sections) = input.C_init(sys_component);
-                    data{x}.h(1:sections) = data{x}.fitfunc(input.Q_init(sys_component));
+%                     data{x}.h(1:sections) = data{x}.fitfunc(input.Q_init(sys_component));
+                    data{x}.h(1:sections) = lut_func(input.Q_init(sys_component),data{x}.lut);
                 else
                     if piping(x).lat_inflow == 1
                         data{x}.Q(1,1:sections) = data{x-1}.Q(1,end)+input.lat.Q{x+pipe_nr};
                         data{x}.C(1,1:sections) = (data{x-1}.C(m,end)*data{x-1}.Q(m,end)+input.lat.C{x-1}*input.lat.Q{x-1})/(data{x-1}.Q(m,end)+input.lat.Q{x-1});
                         
                         data{x}.h(1:sections) = data{x}.fitfunc(data{x}.Q(1,end));
+                        data{x}.h(1:sections) = lut_func(data{x}.Q(1,end),data{x}.lut);
                     else
                         data{x}.Q(1,1:sections) = data{x-1}.Q(1,end);
                         data{x}.C(1,1:sections) = data{x-1}.C(1,end);
                         
-                        data{x}.h(1:sections) = data{x}.fitfunc(data{x}.Q(1,end));
+%                         data{x}.h(1:sections) = data{x}.fitfunc(data{x}.Q(1,end));
+                        data{x}.h(1:sections) = lut_func(data{x}.Q(1,end),data{x}.lut);
                     end
                     %                     data{x}.h(1:sections) = data{x}.fitfunc(data{x-1}.Q(1,end));
                 end
@@ -80,7 +84,8 @@ while abs(avg-desired) > limit
                 if x == 1
                     data{x}.Q(m,n) = input.Q_init(sys_component);
                     data{x}.C(m,n) = input.C_init(sys_component);
-                    data{x}.h(m,n) = data{x}.fitfunc(input.Q_init(sys_component));
+%                     data{x}.h(m,n) = data{x}.fitfunc(input.Q_init(sys_component));
+                    data{x}.h(m,n) = lut_func(input.Q_init(sys_component), data{x}.lut);
                 else
                     if piping(x).lat_inflow == 1
                         data{x}.Q(m,n) = data{x-1}.Q(m,end)+input.lat.Q{x+pipe_nr};
@@ -90,7 +95,8 @@ while abs(avg-desired) > limit
                         data{x}.C(m,n) = data{x-1}.C(m,end);
                     end
 
-                    data{x}.h(m,n) = data{x}.fitfunc(data{x}.Q(m,n));
+%                     data{x}.h(m,n) = data{x}.fitfunc(data{x}.Q(m,n));
+                    data{x}.h(m,n) = lut_func(data{x}.Q(m,n), data{x}.lut);
                 end
                 data{x}.A(m,n) = d^2/4 * acos(((d/2)-data{x}.h(m,n))/(d/2))-sqrt(data{x}.h(m,n)*(d-data{x}.h(m,n)))*((d/2)-data{x}.h(m,n));
                 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
