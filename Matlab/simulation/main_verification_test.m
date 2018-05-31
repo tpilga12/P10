3fafa31402bb52fda38f73ae6283e3e1cdc9bd54
bob <tpilga12@student.aau.dk>
2018-05-31 11:45:03 +0200

    hep

--- Matlab/simulation/main_verification_test.m
+++ Matlab/simulation/main_verification_test.m
@@ -7,12 +7,13 @@ addpath(['functions'], ['setup'], ['input'])
 global Dt iterations error 
 % close all
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-Dt = 20;
-[pipe_spec, nr_pipes, tank_spec, nr_tanks, sys_setup] = pipe_setup_test_verification(1);
+Dt = 15;
+% [pipe_spec, nr_pipes, tank_spec, nr_tanks, sys_setup] = pipe_setup_test_verification(1);
+[pipe_spec, nr_pipes, tank_spec, nr_tanks, sys_setup] = stability_test_setup(1);
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 input.C_init = 8; % initial concentrate in pipe
-input.Q_init = 0.25; % initial input flow
+input.Q_init = 0.15; % initial input flow
 input.tank_height_init(:) = [3 3]; % initial tank height
 for k = 1:length(pipe_spec)
     input.lat.Q{k} = 0;
@@ -26,7 +27,7 @@ tic
 toc
 %% run stuff !!!!!
 clc
-iterations = 10;
+iterations = 60;
 % data = init_data;
 input.C_in = input.C_init;
 input.Q_in = input.Q_init;
@@ -37,8 +38,11 @@ input.Q_in = input.Q_init;
 for m = 2:iterations
         %%%%%% inputs %%%%%%%%%%%%
     input.C_in(m,1) = 8; % concentrate input [g/m^3]
-%     input.Q_in(m,1) = 0.035 + sin(m/10)/35 ;%+ sin(m/100)/15;
-    input.Q_in(m,1) = 0.25;%test_verification_input(m);
+    input.Q_in(m,1) = 0.15;% + sin(m/10)/35 ;%+ sin(m/100)/15;
+if m >= 50
+    input.Q_in(m,1) = 0.35;
+end
+%     input.Q_in(m,1) = 0.25 + sin(m/10)/35;%test_verification_input(m);
     utank1(m,1) = 0.35 + sin(m/10)/65;
     utank2(m,2) = 0.25;
     input.u(m,:) = [utank1(m) utank2(m)]; %input is needed for all actuators, try and remember (look for nr_tanks in workspace) :)
@@ -50,8 +54,12 @@ for m = 2:iterations
 end
 
 %%
+figure(123123)
+hold on
+plot(((1:length(data{1}.h(1,:)))*pipe_spec(1).Dx - pipe_spec(1).Dx), data{1}.h(60,:))
+%%
 sampling = 1; %increase number to skip samples to increase playback speed
-starting_point = 0; % change starting point in iterations (START IS 1)
-playback_speed = 1/2; % 1/fps -> set desired frames per second (warning this is heavily limited by cpu speed)
+starting_point = 40; % change starting point in iterations (START IS 1)
+playback_speed = 1/4; % 1/fps -> set desired frames per second (warning this is heavily limited by cpu speed)
 plot_data(data, nr_tanks, nr_pipes, sys_setup, playback_speed, Dt, pipe_spec, tank_spec, sampling,starting_point)
 
