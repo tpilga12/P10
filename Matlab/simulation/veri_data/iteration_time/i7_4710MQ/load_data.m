0d012ae69d0ffcf17c5d882c5330e296b40a5a23
bob <tpilga12@student.aau.dk>
2018-06-04 00:26:14 +0200

    hep

--- Matlab/simulation/veri_data/iteration_time/i7_4710MQ/load_data.m
+++ Matlab/simulation/veri_data/iteration_time/i7_4710MQ/load_data.m
@@ -5,6 +5,7 @@ ite = {'400' '800' '2000'};
 dt = {'15' '20'};
 meth = {'func' 'lut'};
 cmp = 1;
+perc = 1;
 for m = 1:length(ite)
     for n = 1:length(dt)
         for g = 1:length(meth)
@@ -17,3 +18,18 @@ end
 for n = 1:length(data)
     info{n,2} = data{n}.sim_time;
 end
+
+for n = 2:2:length(info)
+    comp(perc) = info{n-1,2}/info{n,2}-1;    
+     perc = perc + 1;
+end
+comp = comp'.*100;
+
+figure(111231)
+plot(data{1, 1}.input.Q_in)
+grid on
+xlim([0 200])
+ylim([0 0.6])
+ylabel('Flow [m^3/s]')
+xlabel('Iterations')
+
