35b26aa3e0bac22c50af478194e365597d5cf9ef
bob <tpilga12@student.aau.dk>
2018-06-11 23:26:04 +0200

    hep

---
+++ exam_slides/Delete_junk.bat
@@ -0,0 +1,3 @@
+echo off
+rem the next line deletes only files and NOT directories.
+del "*.bbl" del "*.blg" del "*.log" del "*.out" del "*.toc" del "*.aux" del "*.lox" "*.tps" "*.ilg" "*.nlo" "*.synctex.gz" del "*.dvi" del "*.bak" del "*.thm" 
